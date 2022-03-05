01441c3b94952b70ffa64b18737e88de8465f8cf
smarchand-esiea <smarchand@et.esiea.fr>
2022-03-05 13:33:58 +0100

    Add data & style

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,9 +1,22 @@
-import { Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-face-snap',
   templateUrl: './face-snap.component.html',
   styleUrls: ['./face-snap.component.scss']
 })
-export class FaceSnapComponent {
+export class FaceSnapComponent implements OnInit {
+  title!: string;
+  description!: string;
+  createdDate!: Date;
+  snaps!: number;
+  imageUrl!: string;
+
+  ngOnInit() {
+    this.title = 'Laëticia';
+    this.description = 'Ma meilleur amie depuis tout petit !';
+    this.createdDate = new Date();
+    this.snaps = 6;
+    this.imageUrl = 'https://images.pexels.com/photos/7705852/pexels-photo-7705852.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+  }
 }
