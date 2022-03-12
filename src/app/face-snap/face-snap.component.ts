528c4a8456d2d4d5a9b9f69b5506bac11097af58
smarchand-esiea <smarchand@et.esiea.fr>
2022-03-12 21:12:50 +0100

    Add logic services for snap button

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input} from '@angular/core';
 import { FaceSnap } from '../models/face-snap.model';
+import { FaceSnapsService } from '../services/face-snaps.service';
 
 @Component({
   selector: 'app-face-snap',
@@ -7,30 +8,21 @@ import { FaceSnap } from '../models/face-snap.model';
   styleUrls: ['./face-snap.component.scss']
 })
 export class FaceSnapComponent implements OnInit {
-@Input() faceSnap!: FaceSnap;
-
-  title!: string;
-  description!: string;
-  createdDate!: Date;
-  snaps!: number;
-  imageUrl!: string;
+  @Input() faceSnap!: FaceSnap;
   buttonText!: string;
 
+  constructor(private faceSnapsService : FaceSnapsService){
+  }
   ngOnInit() {
-    this.title = 'Laëticia';
-    this.description = 'Ma meilleur amie depuis tout petit !';
-    this.createdDate = new Date();
-    this.snaps = 6;
-    this.imageUrl = 'https://images.pexels.com/photos/7705852/pexels-photo-7705852.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
     this.buttonText = "Oh Snap!";
   }
  onSnap() {
-   if (this.buttonText === 'Oh Snap!') {
-     this.faceSnap.snaps++;
+    if (this.buttonText === 'Oh Snap!') {
+     this.faceSnapsService.snapFaceSnapById(this.faceSnap.id, 'snap');
      this.buttonText = 'Oops, unSnap!';
    } else {
-     this.faceSnap.snaps--;
-     this.buttonText = 'Oh Snap!'
+     this.faceSnapsService.snapFaceSnapById(this.faceSnap.id, 'unsnap');
+     this.buttonText = 'Oh Snap!';
    }
  }
 }
--- src/app/services/face-snaps.service.ts
+++ src/app/services/face-snaps.service.ts
@@ -8,6 +8,7 @@ export class FaceSnapsService{
   faceSnaps:FaceSnap[] =
       [
         {
+          id:1,
           title: 'Laëticia',
           description: 'Ma meilleur amie depuis tout petit !',
           imageUrl: 'https://images.pexels.com/photos/7705852/pexels-photo-7705852.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
@@ -16,6 +17,7 @@ export class FaceSnapsService{
           location: 'Montigny'
         },
         {
+          id:2,
           title: 'Benjamin',
           description: 'Un ami de toute épreuve',
           imageUrl: 'https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
@@ -24,6 +26,7 @@ export class FaceSnapsService{
           location: 'Paris'
         },
         {
+          id:3,
           title: 'Médéric',
           description: 'L\'alsacien',
           imageUrl: 'https://media-exp1.licdn.com/dms/image/C5603AQEb-M9dYahphg/profile-displayphoto-shrink_200_200/0/1634478273983?e=1652313600&v=beta&t=u0gfhwwpoHTbLaCiFQGZcCdIs2UsQgyRYpq6YzOKwJ8',
@@ -31,4 +34,19 @@ export class FaceSnapsService{
           snaps: 0
         }
       ];
+      getAllFaceSnaps(): FaceSnap[] {
+      return this.faceSnaps;
+      }
+      getFaceSnapById(faceSnapId: number): FaceSnap {
+        const faceSnap = this.faceSnaps.find(faceSnap => faceSnap.id === faceSnapId);
+        if (!faceSnap) {
+            throw new Error('FaceSnap not found!');
+        } else {
+            return faceSnap;
+        }
+      }
+      snapFaceSnapById(faceSnapId: number, snapType: 'snap' | 'unsnap'):void {
+        const faceSnap = this.getFaceSnapById(faceSnapId);
+        snapType === 'snap' ? faceSnap.snaps++ : faceSnap.snaps--;
+      }
 }
